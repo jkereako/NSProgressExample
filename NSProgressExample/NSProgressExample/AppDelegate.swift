2bdcf244ede4d67a786035f9e7b5ef7596f64f55
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-10-16 19:07:26 -0400

    Add files

---
+++ NSProgressExample/NSProgressExample/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  NSProgressExample
+//
+//  Created by Jeff Kereakoglow on 10/16/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ NSProgressExample/NSProgressExample/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  NSProgressExample
+//
+//  Created by Jeff Kereakoglow on 10/16/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
